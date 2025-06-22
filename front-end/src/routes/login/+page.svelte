3a296f4a88fa36653748f5e1fae901609b0c83a0
William Yang <willi64645@gmail.com>
2025-06-22 08:02:50 -0400

    styling: login button

--- front-end/src/routes/login/+page.svelte
+++ front-end/src/routes/login/+page.svelte
@@ -118,7 +118,7 @@
         </div>
         <div class="space-y-4">
           <a
-                  class="inline-flex items-center justify-center font-medium transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed bg-[#6DBDD5] text-white hover:shadow-lg px-8 py-4 text-lg rounded-2xl w-full"
+                  class="inline-flex items-center justify-center font-medium transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed bg-[#2eb1d8] text-white hover:shadow-lg px-8 py-4 text-lg rounded-2xl w-full"
                   href="#"
                   on:click|preventDefault={handleLogin}
           >
